820518f0bd1e519c94a26dc471cf704c042ff370
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-04-07 12:35:08 +0700

    [Bernard] Remove unnecessary tags

--- src/CountryComparisonSection.jsx
+++ src/CountryComparisonSection.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import DataBar from './DataBar';
-import styles from './CountryComparison.module.css'
 
 class CountryComparisonSection extends React.Component{
     render(){
         return(
-            <div className={styles.countryComparisonSection}>
+            <div>
                 <DataBar data={this.props.data}/>
             </div>
         );
